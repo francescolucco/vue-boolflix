c2c421beecef9403f13b4e247522df9c29ed0752
francescolucco <francescolucco@googlemail.com>
2021-12-01 12:55:34 +0100

    chiamata API dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Header @titleSearch="titleToSendApi"/>
-    <Main :titleToSend="titleToSend"/>
+    <Main :filmsList="filmsList"/>
 
   </div>
 </template>
@@ -9,12 +9,19 @@
 <script>
 import Header from "./components/Header";
 import Main from "./components/Main";
+import axios from 'axios';
+
 
 export default {
   name: 'App',
     data(){
     return{
-      titleToSend: ''
+      titleToSend: '',
+      urlTheMovied: 'https://api.themoviedb.org/3/search/movie',
+      apiKey: '27d121d27b7dc4f651e4c2ccb0187202',
+      languageIt: '&language=it-IT',
+      languageEn: '&language=en-US',
+      filmsList: []
     }
   },
   components: {
@@ -24,9 +31,26 @@ export default {
   methods:{
     titleToSendApi(text){
       this.titleToSend = text;
+       if(this.titleToSend !== ''){
+         axios.get(this.urlTheMovied,{
+             params:{
+               api_key: this.apiKey,
+               query: this.titleToSend,
+               language: 'it-IT'
+             }
+           })
+         .then(r =>{
+           console.log(r.data.results);
+           this.filmsList = r.data.results;
+         })
+         .catch(e => {
+           console.log(e);
+         })
+       }
     }
   }
 }
+
 </script>
 
 <style lang="scss">
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,47 @@
 <template>
-  <h1>{{titleToSend}}</h1>
+  <div class="box-card-films d-flex justify-content-center flex-wrap">
+    <div 
+      class="card-film"
+      v-for="film in filmsList"
+      :key="film.id"
+      >
+      <h2>Titolo:</h2>
+      <h1>{{film.title}}</h1>
+      <h2>Titolo originale:</h2>
+      <h1>{{film.original_title}}</h1>
+      <h2>Lingua:</h2>
+      <h1>{{film.original_language}}</h1>
+      <h2>Voto:</h2>
+      <h1>{{film.vote_average}}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
    name: "Main",
    props:{
-     titleToSend: String
+     filmsList: Array
    },
-   }
+}
 </script>
 
 <style lang="scss">
+.box-card-films{
+  min-height: calc(100vh - 80px);
+  .card-film{
+    height: 500px;
+    width: 200px;
+    background-color: red;
+    margin: 30px;
+    padding: 20px;
+    h1{
+      font-size: 20px;
+    }
+    h2{
+      color: rgb(251, 188, 14);
+    }
+  }
+}
 </style>
