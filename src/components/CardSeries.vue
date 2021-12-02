f28bc6f7b37d479aaacfe360558d90125a1e83e3
francescolucco <francescolucco@googlemail.com>
2021-12-02 12:57:14 +0100

    aggiunta cover

---
+++ src/components/CardSeries.vue
@@ -0,0 +1,86 @@
+<template>
+      <div 
+         class="flip-card card-film" 
+         v-if="serie.poster_path">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img 
+            :src="`https://image.tmdb.org/t/p/w342${serie.poster_path}`" alt="">
+          </div>
+          <div class="flip-card-back">
+            <h2>Titolo:</h2>
+            <h1>{{serie.name}}</h1>
+            <h2>Titolo originale:</h2>
+            <h1>{{serie.original_name}}</h1>
+            <h2>Lingua:</h2>
+            <h1>
+              <img  v-if="serie.original_language === 'en'" :src="`https://flagcdn.com/224x168/gb-eng.png`" alt="" srcset="">
+              <img   v-else-if="serie.original_language !== 'en'" :src="`https://flagcdn.com/224x168/${serie.original_language}.png`" alt="" srcset="">
+              <div v-else>flag non trovata</div>
+            </h1>
+            <h2>Voto:</h2>
+            <h1>{{serie.vote_average}}</h1>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+
+   export default {
+      name: 'CardSeries',
+      props:{
+         serie: Object
+      }
+}
+</script>
+
+<style lang="scss">
+  .flip-card {
+    background-color: transparent;
+    width: 300px;
+    height: 300px;
+    perspective: 1000px;
+  }
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  }
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+  .flip-card-front, .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+  .flip-card-front {
+    background-color: #bbb;
+    color: black;
+  }
+  .flip-card-back {
+    background-color: #2980b9;
+    color: white;
+    transform: rotateY(180deg);
+  }
+  .card-film{
+    h1{
+      font-size: 20px;
+    }
+    h2{
+      color: rgb(251, 188, 14);
+    }
+    h1 img{
+      width: 60%;
+      }
+  }
+
+
+</style>
